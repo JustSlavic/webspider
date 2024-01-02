42bb2e3779b5ad723ff044b4851f82ab25b504af
vradko <justslavic@gmail.com>
2024-01-02 20:47:52 +0400

    Add run command to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,39 @@
+#!/bin/bash
+
 set -e
 
+echo "number of arguments: $#"
 
-gcc code/main.c -o main -I code/based
+mkdir -p build
+mkdir -p bin
+
+if [ $# -eq 0 ]; then
+    command="build"
+else
+    command="$1"
+fi
+
+
+function build() {
+    gcc code/main.c -o bin/webspider -I code/based
+}
+
+function run() {
+    ( cd www && ../bin/webspider )
+}
+
+
+case $command in
+    build)
+        build
+        ;;
+
+    run)
+        run
+        ;;
+
+    *)
+        echo "Could not recognize command '$command'"
+        ;;
+esac
+
