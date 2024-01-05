ab4bc1077428aa471da1c41741d064e62e632297
vradko <justslavic@gmail.com>
2024-01-05 19:35:12 +0400

    Add subcommand debug to build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,26 @@ if [ $# -eq 0 ]; then
 else
     command="$1"
 fi
+subcommand="$2"
 
 os_name=$(uname -s)
 
 function build() {
+    if [ "$subcommand" = "debug" ]; then
+        DEBUG="-DDEBUG -g3"
+    else
+        WARNINGS="-Wall -Werror"
+    fi
+
     case $os_name in
         Darwin | Linux)
-            gcc code/main_webspider.c -o bin/webspider -I code/based -Wall
+            echo -n "gcc code/main_webspider.c -o bin/webspider -I code/based $DEBUG $WARNINGS"
+            gcc code/main_webspider.c -o bin/webspider -I code/based $WARNINGS $DEBUG
+            if [ "$?" -eq 0 ]; then
+                echo "... Success"
+            else
+                echo "... FAIL"
+            fi
             ;;
         *)
             echo "Unrecognazied os name ($os_name)"
