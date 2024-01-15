710e1f4cb0cfa4d2beacdb2b0af5610fd9fbe245
vradko <justslavic@gmail.com>
2024-01-15 17:31:52 +0400

    Fix epoll after adding inspector

--- code/async_queue_epoll.c
+++ code/async_queue_epoll.c
@@ -6,7 +6,7 @@
 struct async_context
 {
     int queue_fd;
-    struct socket_event_data registered_events[MAX_EVENTS];
+    queue__event_data registered_events[MAX_EVENTS];
 };
 
 
@@ -26,13 +26,13 @@ void destroy_async_context(struct async_context *context)
 }
 
 
-int register_socket(struct async_context *context, int socket_to_register, enum socket_event_type type)
+int queue__register(struct async_context *context, int socket_to_register, int type)
 {
     int result = -2;
     for (int i = 0; i < ARRAY_COUNT(context->registered_events); i++)
     {
-        struct socket_event_data *event = context->registered_events + i;
-        if (event->type == SOCKET_EVENT__NONE)
+        queue__event_data *event = context->registered_events + i;
+        if (event->event_type == QUEUE_EVENT__NONE)
         {
             bool to_read  = ((type & SOCKET_EVENT__INCOMING_CONNECTION) != 0) || ((type & SOCKET_EVENT__INCOMING_MESSAGE) != 0);
             bool to_write = ((type & SOCKET_EVENT__OUTGOING_MESSAGE) != 0);
@@ -50,7 +50,7 @@ int register_socket(struct async_context *context, int socket_to_register, enum 
             result = epoll_ctl(context->queue_fd, EPOLL_CTL_ADD, socket_to_register, &reg_event);
             if (result >= 0)
             {
-                event->type = type;
+                event->event_type = type;
                 event->socket_fd = socket_to_register;
             }
             break;
@@ -61,9 +61,9 @@ int register_socket(struct async_context *context, int socket_to_register, enum 
 }
 
 
-struct socket_event_waiting_result wait_for_new_events(struct async_context *context, int milliseconds)
+queue__waiting_result wait_for_new_events(struct async_context *context, int milliseconds)
 {
-    struct socket_event_waiting_result result = {};
+    queue__waiting_result result = {};
 
     struct epoll_event incoming_event;
     int event_count = epoll_wait(context->queue_fd, &incoming_event, 1, milliseconds);
