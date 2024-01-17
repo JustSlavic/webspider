0dd47dc4bc29a020bab108d94b04b1c113817754
vradko <justslavic@gmail.com>
2024-01-18 02:39:32 +0400

    Hide code that is not ready yet

--- code/http.c
+++ code/http.c
@@ -5,24 +5,26 @@
 memory_block http_request_to_blob(http_request);
 memory_block http_response_to_blob(http_response);
 
+bool32 is_newline(char c) { return c == '\n'; }
+
 http_request http_request_from_blob(memory_block blob)
 {
     http_request result = {};
-
+#if 0
     struct lexer lexer = make_lexer(blob);
 
-    bool is_get = eat_cstring(lexer, "GET");
+    bool is_get = eat_cstring(&lexer, "GET");
     if (is_get)
     {
-        char c = get_char(lexer);
+        char c = get_char(&lexer);
         if (c == ' ')
         {
-            consume_until("\n");
+            consume_until(is_newline);
         }
     }
 
 
-
+#endif
     return result;
 }
 
