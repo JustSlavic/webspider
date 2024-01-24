d179c34d34d3baa51b8ed3cfdae4520a030b8d34
vradko <justslavic@gmail.com>
2024-01-25 02:20:56 +0400

    WIP: Add acf

---
+++ code/acf_main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <acf.h>
+
+
+char config[] =
+"id = 42"
+"";
+
+
+int main()
+{
+    void *memory = malloc(MEGABYTES(1));
+    memory_block buffer = { .memory = memory, .size = MEGABYTES(1) };
+    memory_allocator arena = make_memory_arena(buffer);
+    memory_block source = { .memory = (byte *) config, .size = ARRAY_COUNT(config) };
+
+    acf *result = acf__parse(arena, source);
+    if (acf__is_null(result))
+    {
+        printf("null\n");
+    }
+    else if (acf__is_integer(result))
+    {
+        printf("%lld\n", acf__get_integer(result));
+    }
+
+    return 0;
+}
+
+
+#include <acf.c>
+#include <memory_allocator.c>
+#include <lexer.c>
